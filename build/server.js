00ea359c0f80b504408b0b4e3c40ad07d5e6ecd6
KanishkaSoni11 <90636506+KanishkaSoni11@users.noreply.github.com>
2022-02-04 20:37:52 -0500

    hw1 edit

--- build/server.js
+++ build/server.js
@@ -4,8 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const UserController_1 = __importDefault(require("./controllers/UserController"));
+const UserDao_1 = __importDefault(require("./daos/UserDao"));
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/tuiter');
 const app = (0, express_1.default)();
 app.get('/hello', (req, res) => res.send('Hello World!'));
 app.get('/add/:a/:b', (req, res) => res.send(req.params.a + req.params.b));
+const userController = new UserController_1.default(app, new UserDao_1.default());
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
--- server.ts
+++ server.ts
@@ -1,4 +1,9 @@
 import express, {Request, Response} from 'express';
+import UserController from "./controllers/UserController";
+import UserDao from "./daos/UserDao";
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/tuiter');
 const app = express();
 
 app.get('/hello', (req: Request, res: Response) =>
@@ -6,6 +11,9 @@ app.get('/hello', (req: Request, res: Response) =>
 
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
+
+const userController = new UserController(app, new UserDao());
+
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
