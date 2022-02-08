4a630242c1f7f05fbe0b499743fbf1830b215314
KanishkaSoni11 <90636506+KanishkaSoni11@users.noreply.github.com>
2022-02-08 14:48:06 -0500

    hw1

--- mongoose/TuitSchema.ts
+++ mongoose/TuitSchema.ts
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
 const TuitSchema = new mongoose.Schema({
     tuit: {type: String, required: true},
     postedOn: Date,
-    postedBy: String,
+    postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
 }, {collection: 'tuits'});
 export default TuitSchema
