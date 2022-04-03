8b501f996bb0c87c9276bac47f4e10f88fff25aa
KanishkaSoni11 <90636506+KanishkaSoni11@users.noreply.github.com>
2022-04-02 21:33:51 -0400

    hw

--- services/AuthenticationService.ts
+++ services/AuthenticationService.ts
@@ -3,15 +3,18 @@ import mongoose from "mongoose";
 
 const userDao: UserDao = UserDao.getInstance();
 
-const PROTOCOL = "mongodb+srv";
-const DB_USERNAME = "giuseppi";//process.env.DB_USERNAME;
-const DB_PASSWORD = "supersecretpassword";//process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-const DB_NAME = "myFirstDatabase";
-const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
-// connect to the database
-mongoose.connect(connectionString);
+// const PROTOCOL = "mongodb+srv";
+// const DB_USERNAME = "giuseppi";//process.env.DB_USERNAME;
+// const DB_PASSWORD = "supersecretpassword";//process.env.DB_PASSWORD;
+// const HOST = "cluster0.m8jeh.mongodb.net";
+// const DB_NAME = "myFirstDatabase";
+// const DB_QUERY = "retryWrites=true&w=majority";
+// const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+// // connect to the database
+// mongoose.connect(connectionString);
+
+const connectionString = `mongodb+srv://kanishkasoni:kanu17@cluster0.ilo4h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+mongoose.connect(connectionString)
 
 
 export const login = (u: string, p: string) =>
